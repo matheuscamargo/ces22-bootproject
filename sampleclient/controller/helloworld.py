5f3fcd411ce81f0a728921b929ba9b2692d39f85
alemuzio@aluno.ita.br <alemuzio@aluno.ita.br>
2015-03-16 17:59:06 -0300

    Adding simple client logic

--- sampleclient/controller/helloworld.py
+++ sampleclient/controller/helloworld.py
@@ -1,15 +1,30 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__,  template_folder='../templates')
 
 @app.route('/<username>')
 def hello_world(username):
     return render_template('index.html', name=username)
 
 @app.route('/')
-def index():
+def home():
 	return render_template('index.html')
+
+
+# Method related to the External API
+@app.route('/add')
+def add():
+	return render_template('add.html')
+
+@app.route('/modify')
+def modify():
+	return render_template('modify.html')
+
+@app.route('/list')
+def list():
+	return render_template('list.html')
+
 
 if __name__ == '__main__':
 	app.debug = True
