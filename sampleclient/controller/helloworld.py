850c5d90b3d618c10ceb9eebde6ffd0de35f7f91
alemuzio@aluno.ita.br <alemuzio@aluno.ita.br>
2015-03-20 21:14:08 -0300

    Adding controller and template logic

--- sampleclient/controller/helloworld.py
+++ sampleclient/controller/helloworld.py
@@ -1,29 +1,45 @@
 from flask import Flask
 from flask import render_template
+import requests
+import json
+
 
 app = Flask(__name__,  template_folder='../templates')
+
+json_api_string = "http://192.168.0.22:8080/api"
 
 @app.route('/<username>')
 def hello_world(username):
     return render_template('index.html', name=username)
 
-@app.route('/')
-def home():
-	return render_template('index.html')
-
+# @app.route('/')
+# def home():
+# 	return render_template('index.html')
 
 # Method related to the External API
-@app.route('/add')
-def add():
+@app.route('/add', methods = ['GET'])
+def show_add_form():
 	return render_template('add.html')
 
-@app.route('/modify')
-def modify():
-	return render_template('modify.html')
+@app.route('/add', methods = ['POST'])
+def add():
+	r = requests.post(json_api_string)
+	print json.dumps(r.json())
+	return redirect(url_for('list'))
 
-@app.route('/list')
+@app.route('/modify/<id>')
+def modify():
+	request = requests.get(json_api_string + "/string")
+	request_json = request.json()
+
+	return render_template('modify.html', entry=request_json)
+
+@app.route('/', methods = ['GET'])
 def list():
-	return render_template('list.html')
+	# request = requests.get(json_api_string)
+	# request_json = request.json()
+	return render_template('index.html')
+	return render_template('index.html', entries=request_json)
 
 
 if __name__ == '__main__':
